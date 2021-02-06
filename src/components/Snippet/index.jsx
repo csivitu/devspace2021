b92b2ed14c0293ae08e43f9b7562458312b14f84
Subham Panda <im.sspanda10@gmail.com>
2021-02-06 23:13:24 +0530

    fix: home section and about section with responsiveness

--- src/components/Snippet/index.jsx
+++ src/components/Snippet/index.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import './tinymessage.css'
-import {Container,Row,Col} from 'react-bootstrap';
+import './styles.css';
+import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const tinymessage = (props) => (
-        <Container id="main" className="snippet-conteiner">
-            <Row className="justify-content-center">
-               <Col  sm={6} lg={8}> <h2 id="title">{props.title}</h2></Col>
-            </Row>
-            <Row className="justify-content-center">
-               <Col sm={6} lg={8}> 
-                    <p id="message">
-                        {props.message}
-                    </p>
-                </Col>
-            </Row>
-        </Container>
-)
+    <Container id="main" className="px-5 py-4 m-4">
+        <Row className="justify-content-center">
+            <Col sm={8}>
+                {' '}
+                <h2 id="title">{props.title}</h2>
+            </Col>
+        </Row>
+        <Row className="justify-content-center">
+            <Col sm={8}>
+                <p id="message">{props.message}</p>
+            </Col>
+        </Row>
+    </Container>
+);
 
 export default tinymessage;
--- src/sections/About/index.jsx
+++ src/sections/About/index.jsx
@@ -1,35 +1,59 @@
 import React from 'react';
-import {Container,Row,Col,Carousel} from 'react-bootstrap'
-import './about.css'
-import Snippet from '../../components/Snippet' 
+import { Container, Row, Col, Carousel } from 'react-bootstrap';
+import './styles.css';
+import Snippet from '../../components/Snippet';
 import caroselitem1 from '../../img/home-jumbotron-bg.png';
-import 'bootstrap/dist/css/bootstrap.min.css'
-const data = [{title: 'HACKATHON',message: 'fsdoifjopisdjfsdjfopasjfpojksadopf\nksadpoisadjfoisjfioasjdfjsfjspofdfdaf       asedfsdfsdasfadfasdfsdfsdfsdf'},
-            {title: 'HACKATHON',message: 'fsdoifjopisdjfsdjfopasjfpojksadopf\nksadpoisadjfoisjfioasjdfjsfjspofdfdaf       asedfsdfsdasfadfasdfsdfsdfsdf'},
-            {title: 'HACKATHON',message: 'fsdoifjopisdjfsdjfopasjfpojksadopf\nksadpoisadjfoisjfioasjdfjsfjspofdfdaf       asedfsdfsdasfadfasdfsdfsdfsdf'}]
+import 'bootstrap/dist/css/bootstrap.min.css';
+const data = [
+    {
+        title: 'HACKATHON',
+        message:
+            'fsdoi fjopis djfsd jfop asjf pojksad opf ksadpoisadj foisjfi oasjdf jsfjspof dfdaf asedfs fsdasfadf fsdoi fjopis djfsd jfop asjf pojksad fsdoi fjopis djfsd jfop asjf pojksad ',
+    },
+    {
+        title: 'HACKATHON',
+        message:
+            'fsdoi fjopis djfsd jfop asjf pojksad opf ksadpoisadj foisjfi oasjdf jsfjspof dfdaf asedfs fsdasfadf fsdoi fjopis djfsd jfop asjf pojksad fsdoi fjopis djfsd jfop asjf pojksad ',
+    },
+    {
+        title: 'HACKATHON',
+        message:
+            'fsdoi fjopis djfsd jfop asjf pojksad opf ksadpoisadj foisjfi oasjdf jsfjspof dfdaf asedfs fsdasfadf fsdoi fjopis djfsd jfop asjf pojksad fsdoi fjopis djfsd jfop asjf pojksad ',
+    },
+];
 export default function About() {
     return (
         <Container fluid className="vw-100">
-           <Row id="parentRow">
+            <Row id="parentRow">
                 <Col md={6} id="left">
-                    <Container>
-                            {data.map((element,index)=>(
-                                 <Row key={index}>
-                                    <Col style={{marginTop: 16}}>
-                                        <Snippet title={element.title} message={element.message}/>
-                                    </Col>
-                                </Row>))}
-                    </Container>
+                    {data.map((element, index) => (
+                        <Row className="d-flex justify-content-center align-items-center" key={index}>
+                            {/* <Col style={{ marginTop: 16 }}> */}
+                                <Snippet
+                                    title={element.title}
+                                    message={element.message}
+                                />
+                            {/* </Col> */}
+                        </Row>
+                    ))}
                 </Col>
                 <Col md={6} id="right">
-                    <Carousel style={{height: '100%'}}>
+                    <Carousel>
                         <Carousel.Item>
-                            <img src={caroselitem1} alt="Previous Devspace event Images"/>
+                            <img
+                                src={caroselitem1}
+                                alt="Previous Devspace event Images"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                src={caroselitem1}
+                                alt="Previous Devspace event Images"
+                            />
                         </Carousel.Item>
                     </Carousel>
                 </Col>
-           </Row>
+            </Row>
         </Container>
     );
-};
-
+}
